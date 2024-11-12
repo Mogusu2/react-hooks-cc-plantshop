d3d0591813cbd6e68051e818a07dbfe4845860ec
James Maroko <jamiemaroko41@gmail.com>
2024-11-12 12:47:26 +0300

    Modified PlantPage.js

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, [])
+
   return (
-    <main>
-      <NewPlantForm />
+    <div className="plant-page">
       <Search />
+      <NewPlantForm />
       <PlantList />
-    </main>
+    </div>
   );
 }
 
